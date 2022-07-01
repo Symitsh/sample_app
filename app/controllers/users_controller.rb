1e90605a0993217e4a67fb1ed2201f4d9d5958c5
Symitsh <sandri822@hotmail.fr>
2022-07-01 16:09:35 +0200

    ✨ Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # sign_in @user
-      # flash[:success] = "Welcome to the Sample App!"
-      # redirect_to @user
+      flash[:success] = "Bienvenue sur mon site d'exemple!"
+      redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
