aa1732426e4fdc8c1da46b1154eed36489a649f5
Symitsh <sandri822@hotmail.fr>
2022-07-05 08:02:30 +0200

    🚀 Finish user edit, update, index, and destroy actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = users(:michael)
+    @user       = users(:michael)
     @other_user = users(:archer)
   end
 
@@ -30,11 +30,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
-  test "devrait rediriger vers edit lorsqu'il est connecté en tant qu'utilisateur erroné" do
+  test "ne doit pas autoriser la modification de l'attribut admin via le Web" do
     log_in_as(@other_user)
-    get edit_user_path(@user)
-    assert flash.empty?
-    assert_redirected_to root_url
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                    user: { password:              "password",
+                                            password_confirmation: "password",
+                                            admin: true } }
+    assert_not @other_user.reload.admin?
   end
 
   test "devrait rediriger vers update lorsqu'il est connecté en tant qu'utilisateur erroné" do
@@ -42,6 +45,23 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     patch user_path(@user), params: { user: { name: @user.name,
                                               email: @user.email } }
     assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "devrait rediriger destroy lorsqu'il n'est pas connecté" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_response :see_other
+    assert_redirected_to login_url
+  end
+
+  test "devrait rediriger destroy lorsque vous êtes connecté en tant que non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_response :see_other
     assert_redirected_to root_url
   end
 end
