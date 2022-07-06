cbcb6fe76ebb731bed48b7cfec363c4f47646d70
Symitsh <sandri822@hotmail.fr>
2022-07-06 17:37:48 +0200

    💄 Showing microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# Create a main sample user.
+# Créez un exemple d'utilisateur principal.
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
@@ -7,7 +7,7 @@ User.create!(name:  "Example User",
              activated: true,
              activated_at: Time.zone.now)
 
-# Generate a bunch of additional users.
+# Générez un groupe d'utilisateurs(99) supplémentaires.
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
@@ -18,4 +18,11 @@ User.create!(name:  "Example User",
                password_confirmation: password,
                activated: true,
                activated_at: Time.zone.now)
+end
+
+# Générez des microposts pour un sous-ensemble d'utilisateurs.
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(word_count: 5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
