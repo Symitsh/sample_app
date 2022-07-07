54a638c87ef7f4be50d1bff3d1a4078fe4ae7a10
Symitsh <sandri822@hotmail.fr>
2022-07-07 11:56:58 +0200

    ✨ Add user micrposts

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -70,3 +70,18 @@ class MicropostSidebarTest < MicropostsInterface
     assert_match "1 micropost", response.body
   end
 end
+
+class ImageUploadTest < MicropostsInterface
+
+  test "devrait avoir un champ de saisie de fichier pour les images" do
+    get root_path
+    assert_select 'input[type=file]'
+  end
+
+  test "devrait pouvoir joindre une image à un micropost" do
+    cont = "This micropost really ties the room together"
+    img = fixture_file_upload('kitten.jpg', 'image/jpeg')
+    post microposts_path, params: { micropost: { content: cont, image: img } }
+    assert assigns(:micropost).image.attached?
+  end
+end
